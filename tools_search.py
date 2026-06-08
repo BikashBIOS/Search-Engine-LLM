4f6a346fa0756f8d59760450d9eb03460e999948
Bikash Ranjan Ojha <53367495+BikashBIOS@users.noreply.github.com>
2026-06-08 20:10:19 +0530

    commit

---
+++ tools_search.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchResults
+from langchain_classic.agents import AgentExecutor, create_react_agent
+from langsmith import Client
+from langchain_community.callbacks import StreamlitCallbackHandler
+import os
+from dotenv import load_dotenv
+
+## Arxiv and wikipedia Tools
+##arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+##arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
+
+search=DuckDuckGoSearchResults()
+
+
+st.title("🔎 LangChain - Chat with search")
+"""
+In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+"""
+
+## Sidebar for settings
+st.sidebar.title("Settings")
+api_key=st.sidebar.text_input("Enter your Groq API Key:",type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[
+        {"role":"assisstant","content":"Hi,I'm a chatbot who can search the web. How can I help you?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if prompt:=st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append({"role":"user","content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm=ChatGroq(groq_api_key=api_key,model_name="llama-3.3-70b-versatile",streaming=True)
+    tools=[search,wiki]
+
+    hub = Client()
+
+    react_prompt = hub.pull_prompt("hwchase17/react",dangerously_pull_public_prompt=True)
+
+    agent = create_react_agent(
+        llm=llm,
+        tools=tools,
+        prompt=react_prompt
+    )
+    search_agent = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        handle_parsing_errors=True,
+        max_iterations=10,
+        early_stopping_method="generate"
+    )
+
+    with st.chat_message("assistant"):
+        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+        response = search_agent.invoke(
+            {"input": prompt},
+            {"callbacks": [st_cb]}
+        )
+        output = response["output"]
+        st.session_state.messages.append({'role':'assistant',"content":output})
+        st.write(output)
